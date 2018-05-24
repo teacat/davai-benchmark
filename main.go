3b41dc9d9dcba49afc07372922ad558b5197f490
Yami Odymel <yamiodymel@gmail.com>
2018-05-24 23:22:38 +0800

    能夠輸出 CSV 數據了

---
+++ main.go
@@ -0,0 +1,483 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"regexp"
+	"time"
+
+	"github.com/astaxie/beego"
+	"github.com/bmizerany/pat"
+	"github.com/dimfeld/httptreemux"
+	"github.com/gin-gonic/gin"
+	"github.com/go-martini/martini"
+	"github.com/gorilla/mux"
+	"github.com/gramework/gramework"
+	"github.com/julienschmidt/httprouter"
+	"github.com/labstack/echo"
+	davai "github.com/teacat/go-davai"
+	"github.com/valyala/fasthttp"
+	"github.com/zenazn/goji"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (this *MainController) Get() {
+	this.Ctx.WriteString("hello world")
+}
+
+func serverStartup() {
+	go nethttpServer([]string{
+		"/",
+		"/users",
+		"/user/:name",
+		"/noise/noise/noise",
+		"/noise/noise/noise/noise",
+		"/noise/noise/noise/noise/noise",
+	})
+	go httprouterServer([]string{
+		"/",
+		"/users",
+		"/user/:name",
+		"/noise/noise/noise",
+		"/noise/noise/noise/noise",
+		"/noise/noise/noise/noise/noise",
+	})
+	go martiniServer([]string{
+		"/",
+		"/users",
+		"/user/:name",
+		"/noise/noise/noise",
+		"/noise/noise/noise/noise",
+		"/noise/noise/noise/noise/noise",
+	})
+	go gojiServer([]string{
+		"/",
+		"/users",
+		"/user/:name",
+		"/noise/noise/noise",
+		"/noise/noise/noise/noise",
+		"/noise/noise/noise/noise/noise",
+	})
+	go patServer([]string{
+		"/",
+		"/users",
+		"/user/:name",
+		"/noise/noise/noise",
+		"/noise/noise/noise/noise",
+		"/noise/noise/noise/noise/noise",
+	})
+	go fasthttpServer([]string{
+		"/",
+		"/users",
+		"/user/:name",
+		"/noise/noise/noise",
+		"/noise/noise/noise/noise",
+		"/noise/noise/noise/noise/noise",
+	})
+	go ginServer([]string{
+		"/",
+		"/users",
+		"/user/:name",
+		"/noise/noise/noise",
+		"/noise/noise/noise/noise",
+		"/noise/noise/noise/noise/noise",
+	})
+	go muxServer([]string{
+		"/",
+		"/users",
+		"/user/{name}",
+		"/noise/noise/noise",
+		"/noise/noise/noise/noise",
+		"/noise/noise/noise/noise/noise",
+	})
+	go grameworkServer([]string{
+		"/",
+		"/users",
+		"/user/:name",
+		"/noise/noise/noise",
+		"/noise/noise/noise/noise",
+		"/noise/noise/noise/noise/noise",
+	})
+	go httptreemuxServer([]string{
+		"/",
+		"/users",
+		"/user/:name",
+		"/noise/noise/noise",
+		"/noise/noise/noise/noise",
+		"/noise/noise/noise/noise/noise",
+	})
+	go echoServer([]string{
+		"/",
+		"/users",
+		"/user/:name",
+		"/noise/noise/noise",
+		"/noise/noise/noise/noise",
+		"/noise/noise/noise/noise/noise",
+	})
+	go davaiServer([]string{
+		"/",
+		"/users",
+		"/user/{name}",
+		"/noise/noise/noise",
+		"/noise/noise/noise/noise",
+		"/noise/noise/noise/noise/noise",
+	})
+	go beegoServer([]string{
+		"/",
+		"/users",
+		"/user/:name",
+		"/noise/noise/noise",
+		"/noise/noise/noise/noise",
+		"/noise/noise/noise/noise/noise",
+	})
+
+	log.Printf("等待所有伺服器啟動⋯")
+	<-time.After(time.Second * 2)
+}
+
+var (
+	testURLs = []string{
+		// /
+		"/",
+		// /users
+		"/users",
+		// /user/:name
+		"/user/yamiodymel",
+	}
+	testServers = []string{
+		"davai",
+		//"nethttp",
+		"httprouter",
+		"martini",
+		//"goji",
+		"pat",
+		//"fasthttp",
+		"gin",
+		"mux",
+		"gramework",
+		"httptreemux",
+		"echo",
+		"beego",
+	}
+)
+
+func serverBenchmark() {
+	for _, v := range testServers {
+		for _, vv := range testURLs {
+			run(v, vv)
+		}
+	}
+}
+
+func main() {
+	serverStartup()
+	serverBenchmark()
+	serverReport()
+}
+
+func serverReport() {
+	// Create a csv file
+	f, err := os.Create("./people.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	// Write Unmarshaled json data to CSV file
+	w := csv.NewWriter(f)
+
+	w.Write([]string{
+		"RouterName",
+		"TotalTime",
+		"Location",
+		"Goroutines",
+		"TotalRequest",
+		"ActualTime",
+		"ReadSize",
+		"RequestsPerSecond",
+		"TransferPerSecond",
+		"AverageRequestTime",
+		"FastestRequestTime",
+		"SlowestRequestTime",
+		"NumberOfError",
+	})
+
+	var record []string
+	for _, v := range results {
+		record = append(record, v["RouterName"])
+		record = append(record, v["TotalTime"])
+		record = append(record, v["Location"])
+		record = append(record, v["Goroutines"])
+		record = append(record, v["TotalRequest"])
+		record = append(record, v["ActualTime"])
+		record = append(record, v["ReadSize"])
+		record = append(record, v["RequestsPerSecond"])
+		record = append(record, v["TransferPerSecond"])
+		record = append(record, v["AverageRequestTime"])
+		record = append(record, v["FastestRequestTime"])
+		record = append(record, v["SlowestRequestTime"])
+		record = append(record, v["NumberOfError"])
+		w.Write(record)
+		record = []string{}
+	}
+
+	w.Flush()
+}
+
+var (
+	nethttpPort     = ":9086"
+	httprouterPort  = ":9087"
+	martiniPort     = ":9088"
+	gojiPort        = ":9089"
+	patPort         = ":9090"
+	fasthttpPort    = ":9091"
+	ginPort         = ":9092"
+	muxPort         = ":9093"
+	grameworkPort   = ":9094"
+	httptreemuxPort = ":9095"
+	echoPort        = ":9096"
+	davaiPort       = ":9097"
+	beegoPort       = ":9098"
+	results         = []map[string]string{}
+)
+
+func run(server string, path string) {
+	var port string
+	switch server {
+	case "nethttp":
+		port = nethttpPort
+	case "httprouter":
+		port = httprouterPort
+	case "martini":
+		port = martiniPort
+	case "goji":
+		port = gojiPort
+	case "pat":
+		port = patPort
+	case "fasthttp":
+		port = fasthttpPort
+	case "gin":
+		port = ginPort
+	case "mux":
+		port = muxPort
+	case "gramework":
+		port = grameworkPort
+	case "httptreemux":
+		port = httptreemuxPort
+	case "echo":
+		port = echoPort
+	case "davai":
+		port = davaiPort
+	case "beego":
+		port = beegoPort
+	}
+
+	log.Printf("開始測試 %s 伺服器效能。", server)
+
+	out, err := exec.Command("/Users/YamiOdymel/go/bin/go-wrk", "-c", "5", "-d", "1", "-redir", fmt.Sprintf("http://localhost%s%s", port, path)).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	parsed := analyze(string(out))
+	parsed["RouterName"] = server
+	parsed["Location"] = path
+	results = append(results, parsed)
+}
+
+// https://stackoverflow.com/a/30483899/5203951
+func mapSubexpNames(m, n []string) map[string]string {
+	if len(m) == 0 || len(n) == 0 {
+		panic("錯誤")
+	}
+	m, n = m[1:], n[1:]
+	r := make(map[string]string, len(m))
+	for i := range n {
+		r[n[i]] = m[i]
+	}
+	return r
+}
+
+func analyze(res string) map[string]string {
+	r := regexp.MustCompile(`Running (?P<TotalTime>.*) test @ (?P<Location>.*)
+  (?P<Goroutines>.*) goroutine\(s\) running concurrently
+(?P<TotalRequest>.*) requests in (?P<ActualTime>.*), (?P<ReadSize>.*) read
+Requests\/sec:		(?P<RequestsPerSecond>.*)
+Transfer\/sec:		(?P<TransferPerSecond>.*)
+Avg Req Time:		(?P<AverageRequestTime>.*)
+Fastest Request:	(?P<FastestRequestTime>.*)
+Slowest Request:	(?P<SlowestRequestTime>.*)
+Number of Errors:	(?P<NumberOfError>.*)`)
+	m := r.FindStringSubmatch(res)
+	n := r.SubexpNames()
+
+	fmt.Printf("%+v", res)
+
+	return mapSubexpNames(m, n)
+}
+
+func nethttpServer(paths []string) {
+	for _, v := range paths {
+		http.HandleFunc(v, func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, world!\n")
+		})
+	}
+	err := http.ListenAndServe(nethttpPort, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func httprouterServer(paths []string) {
+	httpRouter := httprouter.New()
+	for _, v := range paths {
+		httpRouter.GET(v, func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+			fmt.Fprint(w, "Hello, world!\n")
+		})
+	}
+	err := http.ListenAndServe(httprouterPort, httpRouter)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func martiniServer(paths []string) {
+	martiniRouter := martini.NewRouter()
+	martiniServer := martini.New()
+	for _, v := range paths {
+		martiniRouter.Get(v, func() string {
+			return "Hello, world!\n"
+		})
+	}
+	martiniServer.Action(martiniRouter.Handle)
+	martiniServer.RunOnAddr(martiniPort)
+	//martiniRouter.RunOnAddr(martiniPort)
+}
+
+func gojiServer(paths []string) {
+	for _, v := range paths {
+		goji.Get(v, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, "Hello, world!\n")
+		}))
+	}
+	err := http.ListenAndServe(gojiPort, goji.DefaultMux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func patServer(paths []string) {
+	patRouter := pat.New()
+	for _, v := range paths {
+		patRouter.Get(v, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, "Hello, world!\n")
+		}))
+	}
+	err := http.ListenAndServe(patPort, patRouter)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func fasthttpServer(paths []string) {
+
+	requestHandler := func(ctx *fasthttp.RequestCtx) {
+		fmt.Fprintf(ctx, "Hello, world!\n")
+	}
+
+	err := fasthttp.ListenAndServe(fasthttpPort, requestHandler)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func ginServer(paths []string) {
+	ginRouter := gin.New()
+	for _, v := range paths {
+		ginRouter.GET(v, func(c *gin.Context) {
+			c.String(200, "Hello, world!\n")
+		})
+	}
+	err := ginRouter.Run(ginPort)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func muxServer(paths []string) {
+	muxRouter := mux.NewRouter()
+	for _, v := range paths {
+		muxRouter.HandleFunc(v, func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, "Hello, world!\n")
+		})
+	}
+	err := http.ListenAndServe(muxPort, muxRouter)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func grameworkServer(paths []string) {
+	grameRouter := gramework.New()
+	for _, v := range paths {
+		grameRouter.GET(v, "Hello, world!\n")
+	}
+	err := grameRouter.ListenAndServe(grameworkPort)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func httptreemuxServer(paths []string) {
+	treemuxRouter := httptreemux.New()
+	for _, v := range paths {
+		treemuxRouter.GET(v, func(w http.ResponseWriter, r *http.Request, _ map[string]string) {
+			fmt.Fprint(w, "Hello, world!\n")
+		})
+	}
+	err := http.ListenAndServe(httptreemuxPort, treemuxRouter)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func echoServer(paths []string) {
+	echoRouter := echo.New()
+	for _, v := range paths {
+		echoRouter.GET(v, func(c echo.Context) error {
+			return c.String(200, "Hello, world!\n")
+		})
+	}
+	err := echoRouter.Start(echoPort)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func davaiServer(paths []string) {
+	davaiRouter := davai.New()
+	for _, v := range paths {
+		davaiRouter.Get(v, func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, "Hello, world!\n")
+		})
+	}
+	err := davaiRouter.Run(davaiPort)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func beegoServer(paths []string) {
+	for _, v := range paths {
+		beego.Router(v, &MainController{})
+	}
+	beego.SetLogger("file", `{"filename":"logs/test.log"}`)
+	beego.BeeLogger.DelLogger("console")
+	beego.Run(beegoPort)
+}
--- 資料夾/benchmark_test.go
+++ 資料夾/benchmark_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"testing"
-	"time"
 
 	"github.com/astaxie/beego"
 	"github.com/dimfeld/httptreemux"
@@ -15,14 +14,6 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/labstack/echo"
 	davai "github.com/teacat/go-davai"
-	"github.com/valyala/fasthttp"
-	// The MySQL driver.
-	_ "github.com/go-sql-driver/mysql"
-)
-
-var (
-	dsn = "root:root@/test?charset=utf8"
-	i   = 0
 )
 
 var (
@@ -34,13 +25,8 @@ var (
 	treemuxRouter *httptreemux.TreeMux
 	echoRouter    *echo.Echo
 	davaiRouter   *davai.Router
+	beegoRouter   *beego.App
 )
-
-type user struct {
-	ID       int    `xorm:"ID" db:"ID"`
-	Username string `xorm:"Username" db:"Username"`
-	Password string `xorm:"Password" db:"Password"`
-}
 
 type MainController struct {
 	beego.Controller
@@ -54,11 +40,21 @@ func init() {
 	var err error
 
 	go func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, world!\n")
+		})
+		err = http.ListenAndServe(":9088", nil)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	/*go func() {
 		httpRouter = httprouter.New()
 		httpRouter.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 			fmt.Fprint(w, "Hello, world!\n")
 		})
-		err = http.ListenAndServe(":9090", httpRouter)
+		err = http.ListenAndServe(":9089", httpRouter)
 		if err != nil {
 			panic(err)
 		}
@@ -69,13 +65,14 @@ func init() {
 		martiniRouter.Get("/", func() string {
 			return "Hello, world!\n"
 		})
-		martiniRouter.Run()
+		martiniRouter.RunOnAddr(":9090")
 	}()
 
 	go func() {
 		requestHandler := func(ctx *fasthttp.RequestCtx) {
 			fmt.Fprintf(ctx, "Hello, world!\n")
 		}
+
 		err = fasthttp.ListenAndServe(":9091", requestHandler)
 		if err != nil {
 			panic(err)
@@ -112,7 +109,7 @@ func init() {
 		if err != nil {
 			panic(err)
 		}
-	}()
+	}()*/
 
 	go func() {
 
@@ -125,21 +122,20 @@ func init() {
 			panic(err)
 		}
 	}()
+	/*
+		go func() {
+
+			echoRouter = echo.New()
+			echoRouter.GET("/", func(c echo.Context) error {
+				return c.String(200, "Hello, world!\n")
+			})
+			err = echoRouter.Start(":9096")
+			if err != nil {
+				panic(err)
+			}
+		}()*/
 
 	go func() {
-
-		echoRouter = echo.New()
-		echoRouter.GET("/", func(c echo.Context) error {
-			return c.String(200, "Hello, world!\n")
-		})
-		err = echoRouter.Start(":9096")
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	go func() {
-
 		davaiRouter = davai.New()
 		davaiRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, "Hello, world!\n")
@@ -150,22 +146,12 @@ func init() {
 		}
 	}()
 
-	go func() {
+	/*go func() {
+		beegoRouter = beego.Router("/", &MainController{})
+		beego.Run(":9098")
+	}()*/
 
-		davaiRouter = davai.New()
-		davaiRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprint(w, "Hello, world!\n")
-		})
-		err = davaiRouter.Run(":9097")
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	beego.Router("/", &MainController{})
-	beego.Run()
-
-	<-time.After(1 * time.Second)
+	//<-time.After(1 * time.Second)
 }
 
 type mockResponseWriter struct{}
@@ -184,19 +170,31 @@ func (m *mockResponseWriter) WriteString(s string) (n int, err error) {
 
 func (m *mockResponseWriter) WriteHeader(int) {}
 
-func BenchmarkDavai_Static(b *testing.B) {
-	w := new(mockResponseWriter)
-	r, _ := http.NewRequest("GET", "/", nil)
+func BenchmarkNetHTTP_Static(b *testing.B) {
 	for x := 0; x < b.N; x++ {
-		davaiRouter.ServeHTTP(w, r)
+		_, err := http.Get("http://localhost:9088/")
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func BenchmarkDavai_Static(b *testing.B) {
+	for x := 0; x < b.N; x++ {
+		resp, err := http.Get("http://localhost:9097/")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%+v", resp)
 	}
 }
 
 func BenchmarkTreeMux_Static(b *testing.B) {
-	w := new(mockResponseWriter)
-	r, _ := http.NewRequest("GET", "/", nil)
 	for x := 0; x < b.N; x++ {
-		treemuxRouter.ServeHTTP(w, r)
+		_, err := http.Get("http://localhost:9095/")
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
@@ -213,5 +211,13 @@ func BenchmarkGin_Static(b *testing.B) {
 	r, _ := http.NewRequest("GET", "/", nil)
 	for x := 0; x < b.N; x++ {
 		ginRouter.ServeHTTP(w, r)
+	}
+}
+
+func BenchmarkBeego_Static(b *testing.B) {
+	w := new(mockResponseWriter)
+	r, _ := http.NewRequest("GET", "/", nil)
+	for x := 0; x < b.N; x++ {
+		beegoRouter.Server.Handler.ServeHTTP(w, r)
 	}
 }
